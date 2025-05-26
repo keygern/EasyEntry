49f686ab8a83e8035197e7bd42c7d4588b8a8855
Piotr Baczar <piotr@Piotrs-MacBook-Pro.local>
2025-05-26 15:35:54 -0400

    add health check api route

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ load_dotenv()
 
 def create_app() -> FastAPI:
     app = FastAPI(title="EasyEntry API", version="0.1.0")
+    
+    @app.get("/") # Health-check
+    def read_root():
+        return {"status": "ok"}
+    
     for r in all_routers:
         app.include_router(r)
     return app
