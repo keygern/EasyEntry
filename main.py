c2b59654083c6dd9dd762d984a11c682a6e20886
Piotr Baczar <piotr@Piotrs-MacBook-Pro.local>
2025-05-26 15:48:32 -0400

    add head handler for clean logs

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ load_dotenv()
 
 def create_app() -> FastAPI:
     app = FastAPI(title="EasyEntry API", version="0.1.0")
-    
+
     @app.get("/") # Health-check
     def read_root():
         return {"status": "ok"}
+    
+    @app.head("/", include_in_schema=False) #Head handler for clean logs. body ignored
+    def root_head():
+        return {"status": "ok"}  
     
     for r in all_routers:
         app.include_router(r)
